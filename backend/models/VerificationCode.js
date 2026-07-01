b341b1c13c832d950f344f518ae8e5c2839a73ca
Tarek Seiba <tarekseiba2004@gmail.com>
2026-07-01 15:59:30 -0400

    feat: add UCF email verification endpoints

---
+++ backend/models/VerificationCode.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const {Schema}=mongoose;
+
+const verificationCodeSchema =new Schema({
+    email: {type:String,required:true,lowercase:true},
+    code: {type:String,required:true,},
+    expiresAt: {type: Date,required:true},
+    used:{type:Boolean,default:false}
+},
+   {timestamps:true});
+    verificationCodeSchema.index({email:1,used:1,expiresAt:1});
+    verificationCodeSchema.index({expiresAt:1},{expiresAfterSeconds:0});
+    module.exports=mongoose.model('VerificatonCode',verificationCodeSchema);
+
